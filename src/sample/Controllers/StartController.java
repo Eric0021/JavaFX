9cbec239bf7398e2fc06064fafcb07b0e6404f0c
eric <alader052500@gmail.com>
2020-06-22 19:19:05 +1000

    added structures, controllers, start screen

---
+++ src/sample/Controllers/StartController.java
@@ -0,0 +1,39 @@
+package sample.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class StartController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void CreditAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void QuitAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SettingsAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void StartAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,13 +2,8 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -21,23 +16,36 @@ public class Main extends Application {
         /// Scene hierarchy:
         //  Root node (fxml and VBox) -> Branch node (Another VBox/HBox) -> Leaf node (like labels and buttons)
 
-        primaryStage.setTitle("Super hot");
-        primaryStage.setWidth(800);
-        primaryStage.setHeight(800);
+        Parent start = FXMLLoader.load(getClass().getResource("fxml/start.fxml"));
+        Scene startScene = new Scene(start);
+        primaryStage.setScene(startScene);
 
 
-        /// This controls the way components in the scene are laid out.
-        VBox parent = new VBox();
+//        primaryStage.setTitle("Super hot");
+//        primaryStage.setWidth(800);
+//        primaryStage.setHeight(800);
 
-        /// The position of this label is controlled by parent.
-        Label label1 = new Label("Hello!!");
-        Button button = new Button("Start");
-        parent.getChildren().addAll(label1, button);
 
-        Scene scene = new Scene(parent);
-        scene.setCursor(Cursor.HAND);
-
-        primaryStage.setScene(scene);
+//        /// This controls the way components in the scene are laid out.
+//        VBox parent = new VBox();
+//
+//
+//        // can also use local images
+////        Image image = new Image();
+//        // ImageView iv = new ImageView(image);
+//
+//        /// The position of this label is controlled by parent.
+//        Label label1 = new Label("Hello!!");
+//        Button button = new Button("Start");
+//        ImageView imageView  = new ImageView("https://is2-ssl.mzstatic.com/image/thumb/Purple113/v4/8a/b0/8d/8ab08d69-4c8b-05a5-bfd6-0d2d446a1f42/source/512x512bb.jpg");
+//        Label label2 = new Label("Hype", imageView);
+//
+//        parent.getChildren().addAll(label1, button, label2);
+//
+//        Scene scene = new Scene(parent);
+//        scene.setCursor(Cursor.HAND);
+//
+//        primaryStage.setScene(scene);
 
 
 
